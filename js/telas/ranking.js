62854866c2c14dc1c64ca86a16cbb0ea4412a459
Luidy <luidy.informatica@gmail.com>
2019-07-03 00:30:46 -0300

    Feito a logica do ranking para ordenar e ranking novo

--- js/telas/ranking.js
+++ js/telas/ranking.js
@@ -1,17 +1,15 @@
-class Ranking{
-    constructor(){
-        if(localStorage.getItem("ranking")==null)
-        {
-            this.lista = Array();   
+class Ranking {
+    constructor() {
+        if (localStorage.getItem("ranking") == null) {
+            this.lista = Array();
         }
-        else
-        {
+        else {
             let rankingDoStorage = localStorage.getItem("ranking");
             this.lista = JSON.parse(rankingDoStorage);
         }
     }
 
-    preload(){
+    preload() {
         this.load.image('fundo', 'assets/fases/background.png');
         this.load.image('bordas', 'assets/fases/bordas.png')
         this.load.image('tabela', 'assets/fases/rankingtable.png')
@@ -27,7 +25,7 @@ class Ranking{
         })
     }
 
-    create(){
+    create() {
 
         this.game.add.sprite(0, 0, 'fundo');
         this.estrelas = game.add.group();   ///faz as estrelas colledirem
@@ -40,20 +38,44 @@ class Ranking{
 
         const exitBtn = game.add.button(game.world.centerX - 400, 540, 'botão', this.exit);
 
-   
-        this.contagem = game.add.text(400, 300, "", { fontSize: '25px', fill: 'white'});
-        for(let i=0;i<this.lista.length;i++)
-        {
-            this.contagem.text +=this.lista[i].nome + " -  " + this.lista[i].pontos + "\n";
+        this.msg1 = game.add.text(190, 49, "Jogadores", { fontSize: '21px', fill: 'blue', });
+        this.msg2 = game.add.text(560, 49, "Pts", { fontSize: '21px', fill: 'blue', });
+
+        this.nome = game.add.text(190, 75, "", { fontSize: '21px', fill: 'black', });
+        this.pts = game.add.text(560, 75, "", { fontSize: '21px', fill: 'black', });
+
+    
+
+        // for (let i = 0; i < this.lista.length; i++) {
+        //     for(let i2 = 0; i2 < this.lista.length; i2++){
+        //         if(this.lista[i].pontos < this.lista[i2].pontos){
+        //             this.lista[i].nome = this.lista[i2].nome;
+        //             this.lista[i].pontos = this.lista[i2].pontos;
+        //             i++;
+        //         }
+        //     }
+        // this.nome.text += this.lista[i].nome + "\n";
+        // this.pts.text += this.lista[i].pontos + "\n";
+        // }
+
+        this.lista.sort(this.ordenar);
+
+        for (let i = 0; i < this.lista.length; i++) {
+            this.nome.text += this.lista[i].nome + "\n";
+            this.pts.text += this.lista[i].pontos + "\n";
         }
+
+        // this.nome.text += this.lista[i].nome + "\n";
+        // this.pts.text += this.lista[i].pontos + "\n";
         
-       
+
+        
 
 
     }
 
-    update(){
-        
+    update() {
+
     }
 
 
@@ -64,9 +86,25 @@ class Ranking{
         game.state.start('Menu')
     }
 
-<<<<<<< HEAD
-    gravar(nome, pontos)
-    {
+    carregarEstrelas() {
+        for (let i = 0; i < 1; i++) {
+            let e = this.estrelas.create(Math.random() * 700, Math.random() * 100 * -8, 'estrela');
+            e.body.gravity.y = Math.random() * (100 - 60) + 60;
+            e.body.collideWorldBounds = true;
+            e.body.velocity.x = Math.random() * (200 - (-200)) + (-200);
+            e.body.bounce.setTo(1, 1);
+    
+        }
+    }
+
+    ordenar (a, b) {
+    if (a.pontos > b.pontos)
+        return -1;
+    if (a.pontos < b.pontos)
+        return 1;
+    return 0;
+}
+    gravar(nome, pontos) {
         //Monta a pontuação
         let p = new Pontuacao();
         p.nome = nome;
@@ -78,23 +116,13 @@ class Ranking{
         localStorage.setItem("ranking", rankingDoStorage);
     }
 }
-class Pontuacao
-{
-    constructor()
-    {
+
+class Pontuacao {
+    constructor() {
         this.pontos = 0;
         this.nome = null;
-=======
 
-    carregarEstrelas() {
-        for (let i = 0; i < 1; i++) {
-            let e = this.estrelas.create(Math.random() * 700, Math.random() * 100 * -8, 'estrela');
-            e.body.gravity.y = Math.random() * (100 - 60) + 60;
-            e.body.collideWorldBounds = true;
-            e.body.velocity.x = Math.random() * (200 - (-200)) + (-200);
-            e.body.bounce.setTo(1, 1);
 
         }
->>>>>>> 333713d38b4cc639259f4a52398f0adb857f7648
     }
-}
+ 
